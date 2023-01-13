6d7392d36dd29d49a3dc323f04bf36a339caf9b5
Adriel Amorim <agimenescomercial@gmail.com>
2023-01-13 19:39:44 -0300

    Added Gherkin Scenarios

--- PracticeExercise1/Tests/Data/ExternalAdditionData.cs
+++
@@ -1,9 +0,0 @@
-public class ExternalAdditionData
-{
-    public static IEnumerable<object[]> AdditionData()
-    {
-        yield return new object[] { 1, 2, 3 };
-        yield return new object[] { 5, -3, 2 };
-        yield return new object[] { 0, 0, 0 };
-    }
-}
